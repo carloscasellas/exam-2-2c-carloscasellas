1a9f714430cc836972808a68355c1092c49d710e
carloscasellas <carloscg10@yahoo.com>
2023-04-06 21:07:27 +0000

    Output looks correct

--- client.c
+++ client.c
@@ -29,33 +29,40 @@ int main(int argc, char *argv[])
     /*
      Read the server port number
     */
-    // TODO
+    portno = atoi(argv[2]);
 
     /*
      Create a socket
     */ 
     // TODO
 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     /*
     Read the server hostname
     */   
     // TODO
+    server = gethostbyname(argv[1]);
 
     /*
     The function bzero() sets all values in a buffer to zero.
     */
     bzero((char *)&server_address, sizeof(server_address));
+    bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
 
     /*
     The variable server_address is a structure of type struct sockaddr_in. This structure has four fields.
     Configure the server_address struct accordingly.
     */
     // TODO
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portno);
 
     /*
     Establish a connection to the server using the socket and the server_address configured above.
     */
     // TODO
+    connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address));
     
     /*
     Prompt the user to enter a message, use fgets to read the message from stdin.
@@ -69,10 +76,18 @@ int main(int argc, char *argv[])
     */
     // TODO
 
+    n = write(sockfd, buffer, sizeof(buffer));
+    if (n < 0)
+        error("ERROR writing to socket");
+    bzero(buffer, 256);
+
     /*
     Read the reply from the socket.
     */
     // TODO
+    n = read(sockfd, buffer, sizeof(buffer));
+    if (n < 0)
+        error("ERROR reading from socket");
 
     /*
      Print the server response and close the socket
